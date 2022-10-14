a75afa140f0fb1d6c565d4c9748408db666278b7
Veselin Doychev <veselindoi123@gmail.com>
2022-10-14 17:03:15 +0300

    Created Classes-Lab-Folder and added all tasks to it

--- demo.js
+++ demo.js
@@ -1,3 +1,13 @@
-let a = 1.2;
+class Person {
+    constructor (fName, lName) {
+        this.fName = fName;
+        this.lName = lName; 
+    }
 
-console.log(Number.isInteger(a));
+    get fullName() {
+        return `${this.fName} ${this.lName}`;
+    }
+}
+
+let person = new Person('Vesko', 'Doychev');
+console.log(person.fullName)
